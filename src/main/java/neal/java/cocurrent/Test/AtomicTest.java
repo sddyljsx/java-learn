7c5cdbfe43d4bf73546b7ccc6f7f26d698b51bfe
songxun <songxun@xiaomi.com>
2018-05-28 13:47:26 +0800

    atomic test

---
+++ src/main/java/neal/java/cocurrent/Test/AtomicTest.java
@@ -0,0 +1,19 @@
+package neal.java.cocurrent.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicTest {
+
+    public static void main(String[] args) {
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        System.out.println(atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(0,1));
+        System.out.println(atomicInteger.compareAndSet(0,2));
+        System.out.println(atomicInteger.get());
+        System.out.println(atomicInteger.addAndGet(1));
+
+    }
+
+
+}
