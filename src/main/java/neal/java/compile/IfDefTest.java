3a0c71933fbc80c652b69cf8057397080857dd12
songxun <songxun@xiaomi.com>
2018-06-04 10:11:58 +0800

    ifdef test

---
+++ src/main/java/neal/java/compile/IfDefTest.java
@@ -0,0 +1,11 @@
+package neal.java.compile;
+
+public class IfDefTest {
+    public static void main(String[] args){
+        if(true){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
