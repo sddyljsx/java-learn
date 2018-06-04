3ec98fb6ed8e2be2f53a8a65d57546eafa38b1dc
songxun <songxun@xiaomi.com>
2018-06-04 09:59:19 +0800

    generic test

---
+++ src/main/java/neal/java/GenericTest.java
@@ -0,0 +1,33 @@
+package neal.java;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GenericTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("a", "b");
+        System.out.println(map.get("a"));
+    }
+
+//    public void test(List<String> list){
+//        System.out.println("invoke list string");
+//    }
+//
+//    public void test(List<Integer> list){
+//        System.out.println("invoke list integer");
+//    }
+
+//    public static String test(List<String> list) {
+//        System.out.println("invoke list string");
+//        return "";
+//    }
+//
+//    public static int test(List<Integer> list) {
+//        System.out.println("invoke list integer");
+//        return 1;
+//    }
+
+
+}
