adaa79d2e3a32beecee6412738e9d40b2022f0d7
songxun <songxun@xiaomi.com>
2018-06-07 16:25:32 +0800

    12

--- src/main/java/neal/java/effectivejava/_3commonmethods/_12.java
+++ src/main/java/neal/java/effectivejava/_3commonmethods/_12.java
@@ -4,4 +4,27 @@ package neal.java.effectivejava._3commonmethods;
  * Item	12:	Always	override	toString
  */
 public class _12 {
+   static class Test {
+        private int a;
+        private String b;
+        private Object c;
+
+        @Override
+        public String toString() {
+            return "Test{" +
+                    "a=" + a +
+                    ", b='" + b + '\'' +
+                    ", c=" + c +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args){
+        Test test = new Test();
+        test.a=1;
+        test.b="b";
+        test.c=new Object();
+        System.out.println(test);
+    }
+
 }
