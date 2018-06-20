f9281d6aeda7525fab17e212a852fe869f209fe8
neal <neal_song@126.com>
2018-06-20 20:23:59 +0800

    daily test

---
+++ src/main/scala/neal/scalatest/DailyTest.scala
@@ -0,0 +1,11 @@
+package neal.scalatest
+
+object DailyTest {
+
+  def main(args: Array[String]): Unit = {
+    println(100.max(1).min(10))
+    println(-1.max(1).min(10))
+    println(5.max(1).min(10))
+  }
+
+}
