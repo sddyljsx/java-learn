b81a77f2f328fe10813c8de29c0cc1b557bb856f
neal <neal_song@126.com>
2018-07-19 11:05:47 +0800

    order test

---
+++ src/main/scala/neal/scalatest/OrderTest.scala
@@ -0,0 +1,20 @@
+package neal.scalatest
+
+
+object OrderTest {
+
+//  def maxListImpParm[Int](elements: List[Int])(implicit orderer: Int => Ordered[Int]): Int =
+//    elements match {
+//      case List() => throw new IllegalArgumentException("empty list!")
+//      case List(x) => x.asInstanceOf[Int]
+//      case List(head, _*) =>
+//        val maxRest = maxListImpParm(tail)(orderer)
+//        if (orderer(head) > maxRest) head.asInstanceOf[Int]
+//        else maxRest
+//    }
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
