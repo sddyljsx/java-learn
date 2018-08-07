f1cd01d5fdd777f56af760d10c33e5f6626b1a1c
neal <neal_song@126.com>
2018-08-07 09:57:19 +0800

    sql

---
+++ src/main/scala/neal/scalatest/spark/sql.scala
@@ -0,0 +1,14 @@
+package neal.scalatest
+
+object sql {
+
+//  spark.sql("create table if not EXISTS test (key int, value string) using csv").show()
+//  spark.sql("insert into table test select 1, 'a'").show()
+//  spark.sql("select * from test").show()
+//  spark.sql("create view test_view as select * from test").show()
+//  spark.sql("select * from test_view").show()
+//  spark.sql("ALTER view test_view as insert into table test select 2, 'b'").show()
+//  spark.sql("select * from test_view").show()
+//  spark.sql("select * from test").show()
+
+}
